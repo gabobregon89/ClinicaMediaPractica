48e29ae7fde150b42b7b3d9e1b9e56cd5050ba68
Gabriel Obregon <gab.obregon89@gmail.com>
2025-02-01 17:37:41 -0300

    Commit inicial

---
+++ src/main/java/com/ProyectoPrueba/ClinicaMedia/ClinicaMediaApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoPrueba.ClinicaMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoPrueba/ClinicaMedia/ClinicaMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoPrueba.ClinicaMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
